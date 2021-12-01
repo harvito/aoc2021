8b82ad54777892aa0ae6ad6e35329b6ca2ac7b01
Mike Harvey <mharvey@GENETEC.COM>
2021-12-01 14:12:15 -0500

    comments and cleaning

--- 01/a.js
+++ 01/a.js
@@ -1,21 +1,23 @@
-// Read the file and print its contents.
-var fs = require('fs')
-  , filename = 'input.txt';
+// load fs module
+var fs = require('fs');
+var filename = 'input.txt';
   fs.readFile(filename, 'utf8', function(err, data) {
+    // err handle
     if (err) throw err;
-    console.log('OK: ' + filename);
+    
+    // split into lines
     var lines = data.split(/\r?\n/);
     console.log(lines.length + ' lines of input');
+
     var n = parseInt(lines[0]);
-    console.log('original n: ' + n)
     var count = 0;
 
+    // for of iterates though in order
     for (line of lines) {
-        console.log('line is ' + line);
         this_n = parseInt(line);
-        // console.log('this n: ' + this_n + ' n: ' + n);
         if (this_n > n) { count++ };
-        n = this_n
+
+        n = this_n // ready for next iteration
     }
     console.log('count: ' + count);
   });
--- 01/b.js
+++ 01/b.js
@@ -1,22 +1,25 @@
-// Read the file and print its contents.
-// var fs = require('fs');
+// load fs module
+var fs = require('fs');
 var filename = 'input.txt';
 fs.readFile(filename, 'utf8', function(err, data) {
-if (err) throw err;
-console.log('OK: ' + filename);
-var lines = data.split(/\r?\n/);
-console.log(lines.length + ' lines of input');
-var count = 0;
+    // err handle
+    if (err) throw err;
 
-for (let i = 3; i < lines.length; i++) {
-    n3ago = parseInt(lines[i-3])
-    n2ago = parseInt(lines[i-2])
-    n1ago = parseInt(lines[i-1])
-    thisN = parseInt(lines[i]);
+    // split into lines
+    var lines = data.split(/\r?\n/);
+    console.log(lines.length + ' lines of input');
+    
+    var count = 0;
+    // start at i = 3 so we can look back 2 3-item sums
+    for (let i = 3; i < lines.length; i++) {
+        n3ago = parseInt(lines[i-3])
+        n2ago = parseInt(lines[i-2])
+        n1ago = parseInt(lines[i-1])
+        thisN = parseInt(lines[i]);
 
-    last3Sum = n3ago + n2ago + n1ago
-    this3Sum = n2ago + n1ago + thisN
-    if (this3Sum > last3Sum) { count++ };
-}
-console.log('count: ' + count);
+        last3Sum = n3ago + n2ago + n1ago // the 3-sum before
+        this3Sum = n2ago + n1ago + thisN // this 3-sum
+        if (this3Sum > last3Sum) { count++ };
+    }
+    console.log('count: ' + count);
 });
